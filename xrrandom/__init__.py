a7b30f198e497ead707fe777f70ff488c9001975
seidlj <jakub.seidl@email.cz>
2019-12-10 13:48:41 +0100

    add basics of distributions module

--- xrrandom/__init__.py
+++ xrrandom/__init__.py
@@ -1,3 +1,4 @@
 from .scipy_stats_sampling import sample_distribution, virtually_sample_distribution
 from .sampling import change_virtual_samples
 from .arch_boostrap_gen import bootstrap_samples
+from . import distributions
