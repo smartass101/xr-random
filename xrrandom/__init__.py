03c0c5d4e901c6e9e2198dde98d203c943cd3570
seidlj <jakub.seidl@email.cz>
2020-01-19 16:13:36 +0100

    rename `distributions` module to `stats`

--- xrrandom/__init__.py
+++ xrrandom/__init__.py
@@ -1,5 +1,5 @@
 from .scipy_stats_sampling import sample_distribution, virtually_sample_distribution
 from .sampling import change_virtual_samples
 from .arch_boostrap_gen import bootstrap_samples
-from . import distributions
-from .distributions import sample
+from . import stats
+from .stats import sample
