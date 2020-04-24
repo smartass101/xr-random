ef927a24d08e1bb42a555d691163566f6c6e3596
seidlj <jakub.seidl@email.cz>
2020-04-24 16:49:15 +0200

    Add tests

---
+++ tests/conftest.py
@@ -0,0 +1,26 @@
+"""Common fixtures"""
+import pytest
+import scipy
+from scipy.stats._distr_params import distcont, distdiscrete
+
+from xrrandom.stats import _excluded_distr
+from xrrandom.scipy_stats_gen import distribution_kind
+
+distcont = dict(distcont)
+distdiscrete = dict(distdiscrete)
+
+scipy_distributions = []
+for name, distr in scipy.stats.__dict__.items():
+    if name in _excluded_distr:
+        continue
+    try:
+        kind = distribution_kind(distr)
+    except ValueError:
+        continue
+    scipy_distributions.append(
+        {'name': name, 'distr': distr, 'kind': kind,
+         'params': distcont.get(name) if kind == 'continuous' else distdiscrete.get(name)})
+
+@pytest.fixture(scope='session', params=scipy_distributions)
+def stats_distr(request):
+    return request.param
